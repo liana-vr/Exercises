1cc8adf9efaea54366fcd4c1d5630699fec4924d
lianatime <liana.vanrensburg@gmail.com>
2021-05-18 13:33:22 +1200

    Sorting + Binary Search

--- Exercises_03.cs
+++ Exercises_03.cs
@@ -11,7 +11,8 @@ public class Program {
 	// Tip: You can use `el is IList<object>` to check whether an item is a list or
 	// an integer.
 
-    // O(n) Time | O(d) space - where n is the total number of elements in the array, including sub-elements, and d is the greatest depth of "special" arrays in the array.
+    // O(n) Time | O(d) space - where n is the total number of elements in the array, 
+    // including sub-elements, and d is the greatest depth of "special" arrays in the array.
 	public static int ProductSum(List<object> array) {
 		return productSumHelper(array, 1);
 	}
@@ -114,5 +115,187 @@ public class Program {
 			}
 		}
 		return -1;
+	}
+}
+
+//////////////////////////////////////////////////////////////////
+// BUBBLE SORT
+// Write a function that takes in array of integers and returns a 
+// sorted version of that array by using Bubble Sort Algorithm
+
+////////////////////////// SOLUTION 1 ////////////////////////////
+
+public class Program {
+	// Best: O(n) time | O(1) space
+	// Average: O(n^2) time | O(1) space
+	// Worst: O(n^2) time | O(1) space
+	public static int[] BubbleSort(int[] array) {
+		if (array.Length == 0){
+			return new int[]{};
+		}
+		bool isSorted = false;
+		int counter = 0;
+		while (!isSorted){
+			isSorted = true;
+			for (int i = 0; i < array.Length - 1 - counter; i++){
+				if (array[i] > array[i + 1]){
+					swap(i, i + 1, array);
+					isSorted = false;
+				}
+			}
+			counter++;
+		}
+		return array;
+	}
+	
+	public static void swap(int i, int j, int[] array) {
+		int temp = array[j];
+		array[j] = array[i];
+		array[i] = temp;
+	}
+}
+
+//////////////////////////////////////////////////////////////////
+// INSERTION SORT
+// Write a function that takes in array of integers and returns a 
+// sorted version of that array by using Insertion Sort Algorithm
+
+////////////////////////// SOLUTION 1 ////////////////////////////
+
+public class Program {
+	// Best: O(n) time | O(1) space
+	// Average: O(n^2) time | O(1) space
+	// Worst: O(n^2) time | O(1) space
+	public static int[] InsertionSort(int[] array) {
+		if (array.Length == 0){
+			return new int[] {};
+		}
+		for (int i = 1; i < array.Length; i++) {
+			int j = i;
+			while (j > 0 && array[j] < array[j - 1]) {
+				swap(j, j-1, array);
+				j -= 1;
+			}
+		}
+		return array;
+	}
+	
+	public static void swap(int i, int j, int[] array) {
+		int temp = array[j];
+		array[j] = array[i];
+		array[i] = temp;
+	}
+}
+
+//////////////////////////////////////////////////////////////////
+// SELECTION SORT
+// Write a function that takes in array of integers and returns a 
+// sorted version of that array by using Selection Sort Algorithm
+
+////////////////////////// SOLUTION 1 ////////////////////////////
+
+public class Program {
+	// Best: O(n) time | O(1) space
+	// Average: O(n^2) time | O(1) space
+	// Worst: O(n^2) time | O(1) space
+	public static int[] SelectionSort(int[] array) {
+		if (array.Length == 0) {
+			return new int[]{};
+		}
+		int startIdx = 0;
+		while (startIdx < array.Length - 1){
+			int smallestIdx = startIdx;
+			for (int i = startIdx + 1; i < array.Length; i++){
+				if (array[smallestIdx] > array[i]) {
+					smallestIdx = i;
+				}
+			}
+			swap (startIdx, smallestIdx, array);
+			startIdx++;
+		}
+		return array;
+	}
+	
+	public static void swap (int i, int j, int[] array){
+		int temp = array[j];
+		array[j] = array[i];
+		array[i] = temp;
+	}
+}
+
+//////////////////////////////////////////////////////////////////
+// PALINDROMR CHECK
+// Write a function that takes a non-empty string and returns a 
+// boolean representing whether the string is a palindrome
+
+////////////////////////// SOLUTION 1 ////////////////////////////
+public class Program { // BRUTE FORCE
+	// O(n^2) time | O(n) space
+	public static bool IsPalindrome(string str) {
+		string reversedString = "";
+		for (int i = str.Length - 1; i >= 0; i--){
+			reversedString += str[i];
+		}
+		return str.Equals(reversedString);
+	}
+}
+
+////////////////////////// SOLUTION 2 ////////////////////////////
+public class Program {
+	// O(n) time | O(1) space
+	public static bool IsPalindrome(string str) {
+		int leftIdx = 0;
+		int rightIdx = str.Length -1;
+		while (leftIdx < rightIdx){
+			if (str[leftIdx] != str[rightIdx]){
+				return false;
+			}
+			leftIdx++;
+			rightIdx--;
+		}
+		return true;
+	}
+}
+
+//////////////////////////////////////////////////////////////////
+// CAESAR CIPHER ENCRYPTOR
+// Write a function that returns a new string obtained by shifting every letter 
+// in the input string by k positions in the alphabet, where k is the key.
+
+////////////////////////// SOLUTION 1 ////////////////////////////
+public class Program {
+	// O(n) time | O(n) space
+	public static string CaesarCypherEncryptor(string str, int key) {
+		char[] newLetters = new char[str.Length];
+		int newKey = key % 26;
+		for (int i = 0; i < str.Length; i++){
+			newLetters[i] = getNewLetter(str[i], newKey);
+		}
+		return new string(newLetters);
+	}
+	
+	public static char getNewLetter(char letter, int key){
+		int newLetterCode = letter + key;
+		return newLetterCode <=
+						122 ? (char)newLetterCode : (char)(96 + newLetterCode % 122);
+	}
+}
+
+////////////////////////// SOLUTION 2 ////////////////////////////
+public class Program {
+	// O(n) time | O(n) space
+	public static string CaesarCypherEncryptor(string str, int key) {
+		char[] newLetters = new char[str.Length];
+		int newKey = key % 26;
+		string alphabet = "abcdefghijklmnopqrstuvwxyz";
+		for (int i=0; i < str.Length; i++){
+			newLetters[i] = getNewLetter(str[i], newKey, alphabet);
+		}
+		return new string(newLetters);
+	}
+	
+	public static char getNewLetter(char letter, int key, string alphabet){
+		int newLetterCode = alphabet.IndexOf(letter) + key;
+		return alphabet[newLetterCode % 26];
 	}
 }
